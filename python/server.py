fad1a789b7cfcc799df071bd5f692b381f4a5433
root <root@localhost.(none)>
2015-04-18 21:32:36 +0800

    修改py文件的缩进

--- python/server.py
+++ python/server.py
@@ -23,38 +23,38 @@ define("mysql_password", default="", help="cmdb database password")
 
 class Application(tornado.web.Application):
     def __init__(self):
-	settings = {                                                                                                        
-        "static_path": os.path.join(os.path.dirname(__file__), "../static"),                                        
-        "template_path" : os.path.join(os.path.dirname(__file__), "../template"),                                   
-        "login_url": "/login/",                                                                                     
-        "debug" : True,                                                                                             
-        "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",                                            
-	#"xsrf_cookies":True,                                                                                      
-	}                                                                                                                   
-                                                                                                                    
-	handlers =[                                                                                 
-    		(r"/", index),                                                                                                  
-    		(r"/base/", base),                                                                                              
-    		(r"/shezhi/",shezhi),                                                                                           
-    		(r"/sear/", sear),                                                                                              
-    		(r"/weixiu/",weixiu),                                                                                           
-    		(r"/out/", loginout),                                                                                           
-    		(r"/user/", user),                                                                                              
-    		(r"/login/", login),                                                                                            
-    		(r"/idc_manager/", idc_manager),                                                                                
-    		(r"/setting/", setting),                                                                                        
-    		(r"/mach_info/", mach_info),                                                                                    
-    		(r"/useradd/", useradd),                                                                                    
-        ]                                                                                                          
+	settings = {                                                              
+     "static_path": os.path.join(os.path.dirname(__file__), "../static"),                         
+     "template_path" : os.path.join(os.path.dirname(__file__), "../template"),                    
+     "login_url": "/login/",                                                 
+     "debug" : True,                                                      
+     "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",                          
+	#"xsrf_cookies":True,                                                  
+	}                                                                   
+                                                                    
+	handlers =[                                                
+    		(r"/", index),                                                        
+    		(r"/base/", base),                                                       
+    		(r"/shezhi/",shezhi),                                                    
+    		(r"/sear/", sear),                                                       
+    		(r"/weixiu/",weixiu),                                                    
+    		(r"/out/", loginout),                                                    
+    		(r"/user/", user),                                                       
+    		(r"/login/", login),                                                     
+    		(r"/idc_manager/", idc_manager),                                               
+    		(r"/setting/", setting),                                                    
+    		(r"/mach_info/", mach_info),                                                
+    		(r"/useradd/", useradd),                                                
+     ]                                                             
 
-        tornado.web.Application.__init__(self, handlers, **settings)
+     	tornado.web.Application.__init__(self, handlers, **settings)
 	try:
-            self.db = torndb.Connection(                                                                                
-           	host=options.mysql_host, database=options.mysql_database,                                               
-            	user=options.mysql_user, password=options.mysql_password,charset='utf8')
+         self.db = torndb.Connection(                                               
+        	host=options.mysql_host, database=options.mysql_database,                             
+         	user=options.mysql_user, password=options.mysql_password,charset='utf8')
 	except:
 	    print  "数据库连接不上"
-#####################################                                                                       
+#####################################                                         
 #权限说明：
 # 1管理员
 # 2用户
@@ -66,75 +66,75 @@ class Application(tornado.web.Application):
 def is_admin(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        if not self.current_user :
-            if self.request.method in ("GET", "HEAD"):
-                url = self.get_login_url()
-                if "?" not in url:
-                    if urlparse.urlsplit(url).scheme:
-                        next_url = self.request.full_url()
-                    else:
-                        next_url = self.request.uri
-                    url += "?" + urlencode(dict(next=next_url))
-                self.redirect(url)
-                return
-            raise tornado.web.HTTPError(403)
-	else:
+     if not self.current_user :
+         if self.request.method in ("GET", "HEAD"):
+          url = self.get_login_url()
+          if "?" not in url:
+              if urlparse.urlsplit(url).scheme:
+               next_url = self.request.full_url()
+              else:
+               next_url = self.request.uri
+              url += "?" + urlencode(dict(next=next_url))
+          self.redirect(url)
+          return
+         raise tornado.web.HTTPError(403)
+     else:
 	    role_ID=self.db.get("SELECT role FROM user WHERE user = %s", self.current_user) 
 	    if int(role_ID['role']) != 1:
 	     	self.write("您没有权限,页面即将跳转 ")
 	     	return
 
-        return method(self, *args, **kwargs)
+     return method(self, *args, **kwargs)
     return wrapper
 
-#####################################                                                                       
+#####################################                                         
 
 
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
-        return self.application.db
+     return self.application.db
     def get_current_user(self):
-        return self.get_secure_cookie("user")
+     return self.get_secure_cookie("user")
 
 class index(BaseHandler):
     @is_admin
     def get(self):
-        self.render("index.html")
+     self.render("index.html")
 class sear(BaseHandler):
     @is_admin
     def get(self):
-        self.render("sear.html")
-    def post(self):                                                                                                 
-        search_comment   =self.get_argument("search")  
+     self.render("sear.html")
+    def post(self):                                                          
+     	search_comment   =self.get_argument("search")  
 	s=conn.ip_search(search_comment)
-        remind=conn.title
+     	remind=conn.title
  	for all in s:
 		all	
-        self.render("sear.html",all=all,remind=remind)
+     	self.render("sear.html",all=all,remind=remind)
 class weixiu(BaseHandler):
     @is_admin
     def get(self):
-        self.render("weixiu.html")
+     self.render("weixiu.html")
 class mach_info(BaseHandler):
     @is_admin
     def get(self):
-        self.render("mach_info.html",results=conn.results())
+     self.render("mach_info.html",results=conn.results())
 class idc_manager(BaseHandler):
     @is_admin
     def get(self):
 	new=self.db.query("SELECT * FROM jifang")
-        self.render("idc_manager.html",new=new)
+     	self.render("idc_manager.html",new=new)
 class user(BaseHandler):
     @is_admin
     def get(self):
 	users=self.db.query("SELECT * FROM user")
 	print users
-        self.render("user.html",users = users)
+     	self.render("user.html",users = users)
 class setting(BaseHandler):
     @is_admin
     def get(self):
-        self.render("setting.html")
+     self.render("setting.html")
 class base(BaseHandler):
     @is_admin
     def get(self):
@@ -146,11 +146,11 @@ class loginout(BaseHandler):
 	last_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 	self.db.execute("UPDATE user SET last_time = %s WHERE user = %s" , last_time, self.current_user)
 	self.set_secure_cookie("user"," ")
-        self.clear_cookie("user")
-        self.redirect('/login/', permanent=True)
+     	self.clear_cookie("user")
+     	self.redirect('/login/', permanent=True)
 class login(BaseHandler):
     def get(self):
-        self.render("login.html")
+     self.render("login.html")
     def post(self):
 	name		=self.get_argument("name")
 	pd		=self.get_argument("passwd")
@@ -179,16 +179,15 @@ class shezhi(BaseHandler):
 
 class useradd(BaseHandler):
     def post(self):
-        user  	=self.get_argument("user")
-        passwd	=self.get_argument("passwd")
-        email  	= self.get_argument("email")
-        tel	= self.get_argument("tel")
-        sele 	= self.get_argument("sele")
-        role	=self.get_argument("role")
-	last_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-	sql 	= "INSERT INTO user(user,passwd,email, tel, sele,role,last_time) VALUES ('%s','%s','%s','%s','%s','%s','%s')" %(user,passwd,email,tel,sele,role,last_time)
-	print sql
-        self.db.execute(sql)
+     user  	=self.get_argument("user")
+     passwd	=self.get_argument("passwd")
+     email  	= self.get_argument("email")
+     tel	= self.get_argument("tel")
+     sele 	= self.get_argument("sele")
+     role	=self.get_argument("role")
+     last_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+     sql 	= "INSERT INTO user(user,passwd,email, tel, sele,role,last_time) VALUES ('%s','%s','%s','%s','%s','%s','%s')" %(user,passwd,email,tel,sele,role,last_time)
+     self.db.execute(sql)
 
 
 def main():
